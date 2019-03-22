26b604b144f568c660cca87b10391bafb372a11b
minh nguyen <imnotminh@gmail.com>
2019-03-22 00:26:19 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   num=0
   loop do
-    number_of_times.times. do
+    num+=
       puts phrase
     end
 end
