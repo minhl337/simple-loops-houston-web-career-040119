6a2c0862cbdeb9c30dd95048b108d2c42f89036d
minh nguyen <imnotminh@gmail.com>
2019-03-22 00:38:45 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -24,6 +24,8 @@ def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  number_of_times.times do
+  
 end
 
 def while_iterator(number_of_times)
